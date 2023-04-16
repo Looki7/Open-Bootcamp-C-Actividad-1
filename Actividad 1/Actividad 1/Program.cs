c8c1dd0ce40b7bf3b88cadcefb422c1f17948075
Looki7 <105614532+Looki7@users.noreply.github.com>
2023-04-16 20:57:04 +0100

    Entrega

---
+++ Actividad 1/Actividad 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Actividad 1A
+
+Console.WriteLine("Introduce tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
+
+// Actividad 1B
+
+Console.WriteLine("¿Qué hora es?");
+string hora = Console.ReadLine();
+Console.WriteLine("Asique son las " + hora);
+
